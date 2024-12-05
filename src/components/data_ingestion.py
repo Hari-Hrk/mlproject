1ba0d391937dbdfd74e804f80af2a1d14a5a335f
Hari-Hrk <hrk.harish87@gmail.com>
2024-12-05 20:42:54 +0530

    Model Training Done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,6 +10,8 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 # this data config means => when data inputs given & output data where the stored Ex here is artifacts
 @dataclass
@@ -50,5 +52,7 @@ if __name__ == "__main__":
     obj = DataIngestion()
     train_data,test_data = obj.initiate_data_ingestion()
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
-
+    train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data,test_data)
+    ModelTrainer = ModelTrainer()
+    print(ModelTrainer.initiate_model_trainer(train_arr,test_arr))
+    
